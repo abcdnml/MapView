0b8156cee44e69413713cbd3b39e246a788b4b97
liyi <liyi2@yxrobot.com>
2021-01-18 16:07:12 +0800

    删除mapimp模块

--- app/src/main/java/com/aaa/lib/mapdemo/MainActivity.java
+++ app/src/main/java/com/aaa/lib/mapdemo/MainActivity.java
@@ -1,21 +1,20 @@
 package com.aaa.lib.mapdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
 import android.os.Bundle;
 
-import com.aaa.lib.map.imp.YXMapView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.aaa.lib.map.MapView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    YXMapView mapView;
+    MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mapView=findViewById(R.id.mv_main);
-        mapView.setBackgroundColor(Color.WHITE);
-        mapView.addAreaDivideLineLayer();
+        mapView = findViewById(R.id.mv_main);
     }
 }
